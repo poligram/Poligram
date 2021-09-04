24904f53403ed65f20e63fda41be155b34189fbd
Poligram <blasterboy8888yt@gmail.com>
2021-09-04 13:41:27 +0100

    Better

--- global.js
+++ global.js
@@ -12,19 +12,26 @@ const header = `<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
 const footer = `<div class="container-fluid bg-dark text-light">
 <div class="row font-weight-bolder text-center" id="footer-titles">
-    <div class="col text-left"><h6>Created by:</h6></div>
-    <div class="col"><h6>Acknowledgements:</h6></div>
-    <div class="col"><h6>Other Links:</h6></div>
+    <div class="col"><h6 data-toggle="tooltip" title="Me">Created by:</h6></div>
+    <div class="col"><h6 data-toggle="tooltip" title="Things/People that helped me create this">Acknowledgements:</h6></div>
+    <div class="col"><h6 data-toggle="tooltip" title="Other links">Other Links:</h6></div>
 </div>
 
 <div class="row font-weight-bold text-center" id="footer-titles">
-    <div class="col text-left"><a href="https://github.com/poligram"><h6>Poligram</h6></a></div>
-    <div class="col"><a href="https://www.w3schools.com"><h6>w3schools</h6></a></div>
+    <div class="col"><a href="https://github.com/poligram"><h6 data-toggle="tooltip" title="My github">Poligram</h6></a></div>
+    <div class="col"><a href="https://www.w3schools.com"><h6 data-toggle="tooltip" title="Helping with some of the bootstrap/jQuery code">w3schools</h6></a></div>
+    <div class="col"><a href="https://www.rateyourpets.com/login.php"><h6 data-toggle="tooltip" title="Website to rate pets">Rate Your Pets</h6></a></div>
+</div>
+
+<div class="row font-weight-bold text-center" id="footer-titles">
     <div class="col"><a href=""><h6></h6></a></div>
+    <div class="col"><a href=""><h6></h6></a></div>
+    <div class="col"><a href="https://benjibow.github.io/clooney/"><h6 data-toggle="tooltip" title="So stupid">ClooneyHub</h6></a></div>
 </div>
 </div>`
 
 $(document).ready(function() {
     $("#TopBarNav").html(header);
     $("#FooterInPage").html(footer);
+    $('[data-toggle="tooltip"]').tooltip();
 });
